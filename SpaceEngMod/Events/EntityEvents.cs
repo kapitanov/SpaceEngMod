0b664109dd6033c015d552a03758daa54a64f808
atrevidogk <atrevido@yandex.ru>
2014-11-28 23:44:15 +0300

    Little modified ps1 + Added lost EntityEvents

---
+++ SpaceEngMod/Events/EntityEvents.cs
@@ -0,0 +1,24 @@
+using SPX.Station.Infrastructure.ApiEntities;
+
+namespace SPX.Station.Infrastructure.Events
+{
+    public static class EntityEvents
+    {
+        public static readonly EntityEvent<Sensor, bool> SensorStateChanged = new EntityEvent<Sensor, bool>();
+
+        public static readonly EntityEvent<AbstractPiston, bool> PistonLimitReached = new EntityEvent<AbstractPiston, bool>();
+
+        public static readonly EntityEvent<LandingGear, bool> LandingGearStateChanged = new EntityEvent<LandingGear, bool>();
+
+        public static readonly EntityEvent<ButtonPanel, int> PistonsButtonPressed =new EntityEvent<ButtonPanel, int>();
+        public static readonly EntityEvent<ButtonPanel, int> GearsButtonPressed = new EntityEvent<ButtonPanel, int>();
+        public static readonly EntityEvent<ButtonPanel, int> ConnectorsButtonPressed = new EntityEvent<ButtonPanel, int>();
+        public static readonly EntityEvent<ButtonPanel, int> AutoDoorButtonPressed = new EntityEvent<ButtonPanel, int>();
+
+        public static readonly EntityEvent<ButtonPanel> PistonsButtonUpdate100 = new EntityEvent<ButtonPanel>();
+        public static readonly EntityEvent<ButtonPanel> GearsButtonUpdate100 = new EntityEvent<ButtonPanel>();
+        public static readonly EntityEvent<ButtonPanel> ConnectorsButtonUpdate100 = new EntityEvent<ButtonPanel>(); 
+        public static readonly EntityEvent<ButtonPanel> AutoDoorButtonUpdate100 = new EntityEvent<ButtonPanel>();
+    }
+    
+}
